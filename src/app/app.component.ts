541945859ae04f17d0807e75e2263a5180f07728
Dalgat <doly2004e@yandex.ru>
2020-10-27 13:44:07 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'hmm';
+  firstOperand: number;
+  secondOperand: number;
+  operator: string;
+  result: number;
+  errorMessage: string;
+  doCalc(){
+    if (this.firstOperand != null && this.secondOperand != null && this.operator != null) {
+      if (this.operator == '-') {
+        this.result = this.firstOperand - this.secondOperand;
+        this.errorMessage = 'Нет ошибки';
+      } else if (this.operator == '+') {
+        this.result = this.firstOperand + this.secondOperand;
+        this.errorMessage = 'Нет ошибки';
+      } else if (this.operator == '/') {
+        if (this.secondOperand == 0) {
+          this.errorMessage = 'На ноль делить нельзя';
+          this.result = null;
+        } else {
+          this.result = this.firstOperand / this.secondOperand
+          this.errorMessage = 'Нет ошибки';
+        }
+      } else if (this.operator == '*') {
+        this.result = this.firstOperand * this.secondOperand;
+        this.errorMessage = 'Нет ошибки';
+      }
+    } else {
+      if (this.firstOperand == null) {
+        this.errorMessage = 'Нет первого операнда';
+      } else if (this.secondOperand == null) {
+        this.errorMessage = 'Нет второго операнда';
+      } else if (this.operator == null) {
+        this.errorMessage = 'Нет оператора действия';
+      }
+    }
+  }
 }
